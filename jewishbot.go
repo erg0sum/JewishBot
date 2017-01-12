cc1b1991c9ebda3f6cbe1939d30eaf3a9c65faf8
adamhotz <ahotz@groupon.com>
2017-01-12 00:00:59 -0800

    Load the glossary

--- jewishbot.go
+++ jewishbot.go
@@ -1,13 +1,33 @@
-package jewishbot
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 /*
 A bot for /r/judaism
 */
-
 func ReadGlossary(filename string) (map[string]string, error) {
-	return make(map[string]string), nil
+	var glossary = make(map[string]string)
+	if glossaryFile, err := os.Open(filename); err != nil {
+		return nil, err
+	} else {
+		jsonParser := json.NewDecoder(glossaryFile)
+		if err = jsonParser.Decode(&glossary); err != nil {
+			return nil, err
+		}
+	}
+	return glossary, nil
 }
 
 func main() {
-
+	if glossary, err := ReadGlossary(os.Args[1]); err != nil {
+		fmt.Printf("Error reading glossary: %s\n", err.Error())
+	} else {
+		for key, value := range glossary {
+			fmt.Printf("%s:%s\n", key, value)
+		}
+	}
 }
--- jewishbot_test.go
+++ jewishbot_test.go
@@ -1,5 +1,22 @@
-package jewishbot
+package main
 
-func testReadGlossary {
-  var glossary, err = ReadGlossary("test/")
+import (
+	"testing"
+)
+
+var testFile string = "test/glossary.json"
+
+func TestReadGlossary(t *testing.T) {
+	var glossary, err = ReadGlossary(testFile)
+	if err != nil {
+		t.Error("error should not be null")
+	}
+	if len(glossary) != 2 {
+		t.Error("wrong length for glossary")
+	}
+	if val, ok := glossary["test"]; !ok {
+		t.Error("test should be in glossary")
+	} else if val != "A test string" {
+		t.Error("Bad value for test string")
+	}
 }
